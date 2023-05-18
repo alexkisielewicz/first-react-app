fe359c7e269cdccac040c7a8ac3a6b37df3c65ad
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-05-18 18:48:37 +0000

    complete challenge

--- react-example/src/App.js
+++ react-example/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import StatefulGreeting from './components/StatefulGreeting';
+import "./App.css";
+import NavBarSimple from "./components/NavBarSimple";
 
 function App() {
   return (
     <div className="App">
-      <StatefulGreeting greeting="I'm a stateful class component" />
+      <NavBarSimple />
     </div>
   );
 }
