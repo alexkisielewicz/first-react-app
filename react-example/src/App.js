edf2077c5ce557f31487a7823233d828f6e942bd
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-05-19 11:26:32 +0000

    follow the lessons

--- react-example/src/App.js
+++ react-example/src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import NavBarSimple from "./components/NavBarSimple";
+import NestingComponents from "./components/NestingComponents";
 
 function App() {
   return (
     <div className="App">
-      <NavBarSimple />
+      <NestingComponents />
     </div>
   );
 }
