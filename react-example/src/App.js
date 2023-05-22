5b6c5131dbbc64c58e5c91f7af244c3498396e87
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-05-22 14:06:15 +0000

    follow the lessons

--- react-example/src/App.js
+++ react-example/src/App.js
@@ -1,16 +1,10 @@
 import css from "./App.module.css";
-// import NavBarSimple from "./components/NavBarSimple";
-import Sidebar from "./components/Sidebar";
-import NavBarForm from "./components/NavBarForm";
-import Content from "./components/Content";
+import LifeCyclesCWU from "./components/LifeCyclesCWU";
 
 function App() {
   return (
     <div className={css.App}>
-      {/* Add your components here */}
-      <NavBarForm />
-      <Sidebar />
-      <Content />
+      <LifeCyclesCWU />
     </div>
   );
 }
