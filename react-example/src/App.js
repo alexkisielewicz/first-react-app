ce7b2ffa0bb746ac01788e049e5e8027546a8dcc
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-05-23 11:23:02 +0000

    follow the lessons

--- react-example/src/App.js
+++ react-example/src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
-import SearchBar from "./components/SearchBar";
+import ControlledFormHooks from "./components/ControlledFormHooks";
 
 function App() {
   return <div className="App">
-      <SearchBar />
+      <ControlledFormHooks />
   </div>;
 }
 
